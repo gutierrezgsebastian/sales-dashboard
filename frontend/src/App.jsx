db52977f98ff2e66b69598780b4f396f5123acf3
gutierrezgsebastian <gutierrezgsebastian@outlook.com>
2022-08-23 15:46:50 -0500

    Setting React structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Fragment } from "react";
+import Home from "./pages/Home/Home";
 import "./App.scss";
 
 function App() {
-  return <div className="App">I'm working</div>;
+  return (
+    <Fragment>
+      <Routes>
+        <Route path="/" component={<Home />}>
+          {/* <Route path="*" component={<Home />} /> */}
+        </Route>
+      </Routes>
+    </Fragment>
+  );
 }
 
 export default App;
